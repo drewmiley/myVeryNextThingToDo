03ff9c5a640fde33aa575f8be6f0644308132a9f
Drew Miley <drew.miley@opencastsoftware.com>
2018-08-06 15:52:53 +0100

    Adds ducks

---
+++ src/ducks/actions.js
@@ -0,0 +1,44 @@
+export const mapDispatchToProps = dispatch => {
+    return { fetchData: url => dispatch(itemsFetchData(url)) };
+};
+
+function itemsFetchData(apiRoute) {
+    return dispatch => {
+        dispatch(itemsIsLoading(true));
+
+        fetch(`https://5826ed963900d612000138bd.mockapi.io/items`)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                dispatch(itemsIsLoading(false));
+
+                return response;
+            })
+            .then(response => response.json())
+            .then(items => dispatch(itemsFetchDataSuccess(items)))
+            .catch(() => dispatch(itemsHasErrored(true)));
+    };
+}
+
+function itemsHasErrored(bool) {
+    return {
+        type: 'ITEMS_HAS_ERRORED',
+        hasErrored: bool
+    };
+}
+
+function itemsIsLoading(bool) {
+    return {
+        type: 'ITEMS_IS_LOADING',
+        isLoading: bool
+    };
+}
+
+function itemsFetchDataSuccess(items) {
+    return {
+        type: 'ITEMS_FETCH_DATA_SUCCESS',
+        items
+    };
+}
--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { combineReducers } from 'redux';
-// import { reducers } from './reducers';
+import { items, itemsHasErrored, itemsIsLoading } from './reducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    items,
+    hasErrored: itemsHasErrored,
+    isLoading: itemsIsLoading
+});
 
 export default function configureStore(initialState) {
     return createStore(
