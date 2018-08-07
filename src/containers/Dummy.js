6ca4dcce7ab4f96e56509b456ca5b0f9d1292b36
Drew Miley <drew.miley@opencastsoftware.com>
2018-08-07 12:20:21 +0100

    Cleans up containers

--- src/containers/Dummy.js
+++ src/containers/Dummy.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import {Button, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
 
 export default class Dummy extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Dummy Screen</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Dummy Screen</Text>
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+            </View>
+        );
+    }
 }
